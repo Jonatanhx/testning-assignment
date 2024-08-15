f1c38385a6f949ddd82b23e8e9576fa3dc7a85e2
Jonatan Helander <Jonatanhelander@hotmail.com>
2024-08-15 21:28:08 +0200

    Removed comment

--- src/components/TodoContext.tsx
+++ src/components/TodoContext.tsx
@@ -1,24 +1,20 @@
 import { createContext, ReactNode, useContext, useState } from "react";
 
-// Define the Todo type
 export interface Todo {
   title: string;
   description: string;
   date: string;
 }
 
-// Define the shape of the context
 export interface TodosContextType {
   todos: Todo[];
   addTodo: (newTodo: Todo) => void;
 }
 
-// Create the context with default value as undefined
 export const TodosContext = createContext<TodosContextType | undefined>(
   undefined
 );
 
-// Define the provider component
 export const TodosProvider = ({ children }: { children: ReactNode }) => {
   const [todos, setTodos] = useState<Todo[]>([]);
 
@@ -33,7 +29,6 @@ export const TodosProvider = ({ children }: { children: ReactNode }) => {
   );
 };
 
-// Custom hook to use the TodosContext
 export const useTodos = () => {
   const context = useContext(TodosContext);
   return context;
