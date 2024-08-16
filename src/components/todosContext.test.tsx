ffbf53aff485da78dc39252ea148ebd76eabf8c2
Jonatan Helander <Jonatanhelander@hotmail.com>
2024-08-16 07:21:19 +0200

    cleaned up test in todosContext

--- src/components/todosContext.test.tsx
+++ src/components/todosContext.test.tsx
@@ -1,20 +1,13 @@
 import { render } from "@testing-library/react";
-import { describe, expect, it, vi } from "vitest";
+import { describe, expect, it } from "vitest";
 import { TodosProvider } from "./TodoContext";
 import TodoList from "./TodoList";
 
 describe("TodosContext", () => {
-  it("should throw an error if useTodos is used outside of TodosProvider", () => {
-    const consoleErrorSpy = vi
-      .spyOn(console, "error")
-      .mockImplementation(() => {});
-
+  it("should throw an error if useTodos function from todoContext is used outside of TodosProvider", () => {
     expect(() => render(<TodoList />)).toThrowError(
       "useTodos requires parent being wrapped by TodosProvider"
     );
-
-    // Restore console.error
-    consoleErrorSpy.mockRestore();
   });
 
   it("should not throw an error if TodoList is used inside TodosProvider", () => {
